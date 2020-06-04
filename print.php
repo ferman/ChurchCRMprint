9f4f13654cbbe2b5adb9643a3770e22f67032a14
Justin Pulsifer <justinpulsifer@gmail.com>
2020-06-03 20:51:23 -0700

    forgot to add font in code

--- print.php
+++ print.php
@@ -7,11 +7,11 @@ $black = imagecolorallocate($img, 0, 0, 0);
 
 // DRAW BACKGROUND AND TEXT
 $txt = "Hello World";
-
+$font = "./Lato-Regular.ttf"; 
 imagefilledrectangle($img, 0, 0, 399, 29, $white);
-imagettftext($img, 24, 0, 5, 24, $grey, $txt);
-imagettftext($img, 24, -10, 5, 120, $black, $txt);
-imagettftext($img, 24, 10, 120, 120, $black, $txt);
+imagettftext($img, 24, 0, 5, 24, $grey, $font, $txt);
+imagettftext($img, 24, -10, 5, 120, $black, $font, $txt);
+imagettftext($img, 24, 10, 120, 120, $black, $font, $txt);
 
 // OUTPUT
 header('Content-type: image/png');
