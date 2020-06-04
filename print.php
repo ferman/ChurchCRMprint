cf2b8b506584244f4f2481aeb5b8b50eb7fcfa02
Justin Pulsifer <justinpulsifer@gmail.com>
2020-06-03 20:46:49 -0700

    create imaget template

--- print.php
+++ print.php
@@ -1,7 +1,24 @@
 <?php
-print_r(shell_exec('dir'));
+// CREATE NEW IMAGE + SPECIFY COLORS
+$img = imagecreate(500, 300);
+$white = imagecolorallocate($img, 255, 255, 255);
+$grey = imagecolorallocate($img, 100, 100, 100);
+$black = imagecolorallocate($img, 0, 0, 0);
 
-shell_exec('brother_ql -b network -m QL-820NWB -p tcp://192.168.1.80 print -l 62 /var/www/churchcrmprint/label.png');
+// DRAW BACKGROUND AND TEXT
+$txt = "Hello World";
+$font = "C:\Windows\Fonts\arial.ttf"; 
+imagefilledrectangle($img, 0, 0, 399, 29, $white);
+imagettftext($img, 24, 0, 5, 24, $grey, $font, $txt);
+imagettftext($img, 24, -10, 5, 120, $black, $font, $txt);
+imagettftext($img, 24, 10, 120, 120, $black, $font, $txt);
+
+// OUTPUT
+header('Content-type: image/png');
+imagepng($img, "test.png");
+imagedestroy($img);
+
+//shell_exec('brother_ql -b network -m QL-820NWB -p tcp://192.168.1.80 print -l 62 /var/www/churchcrmprint/label.png');
 
 echo "done";
 ?>
